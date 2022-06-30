2a3576313b64d717efdc824406eb1df942b50161
HSYNCodeAcademy <99787133+HSYNCodeAcademy@users.noreply.github.com>
2022-06-30 07:18:40 +0400

    Add files via upload

---
+++ DAL/Base/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
