6332281160e4496e8d196d1f9265e813d23f6316
HSYNCodeAcademy <99787133+HSYNCodeAcademy@users.noreply.github.com>
2022-06-30 07:20:36 +0400

    Add files via upload

---
+++ ViewModels/SliderVMs/SliderCreate.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EduhomePraktika._1.ViewModels
+{
+    public class SliderCreate
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string ImageURL { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
